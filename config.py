f06c9920d17f9ea03a4b7398203d609064c9768f
ScRiPt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-10-05 03:19:59 -0700

    update

--- config.py
+++ config.py
@@ -2,27 +2,24 @@ import os
 import urllib
 dir_path = os.path.dirname(os.path.realpath(__file__))
 def ins()
-	os.system("C:\Python27\Scripts\pip.exe install pyinstaller")
-	os.system("C:\Python27\Scripts\pip.exe install simplejson")
-	os.system("C:\Python27\Scripts\pip.exe install pyperclip")
-	os.system("C:\Python27\Scripts\pip.exe install browsercookie")
-	os.system("C:\Python27\Scripts\pip.exe install pynput")
-	os.system("C:\Python27\Scripts\pip.exe install requests")
-	os.system("C:\Python27\Scripts\pip.exe install urllib")
-	os.system("C:\Python27\Scripts\pip.exe install ctypes")
-	os.system("C:\Python27\Scripts\pip.exe install sqlite3")
-	os.system("C:\Python27\Scripts\pip.exe install urllib")
-	os.system("C:\Python27\Scripts\pip install pypiwin32")
-	os.system("C:\Python27\Scripts\pip install logging")
-	os.system("C:\Python27\Scripts\pip install logging")
-	os.system("C:\Python27\Scripts\pip install pyCrypto")
+	os.system("C:\\Python27\\Scripts\\pip.exe install pyinstaller")
+	os.system("C:\\Python27\\Scripts\\pip.exe install simplejson")
+	os.system("C:\\Python27\\Scripts\\pip.exe install pyperclip")
+	os.system("C:\\Python27\\Scripts\\pip.exe install browsercookie")
+	os.system("C:\\Python27\\Scripts\\pip.exe install pynput")
+	os.system("C:\\Python27\\Scripts\\pip.exe install requests")
+	os.system("C:\\Python27\\Scripts\\pip.exe install urllib")
+	os.system("C:\\Python27\\Scripts\\pip.exe install ctypes")
+	os.system("C:\\Python27\\Scripts\\pip.exe install sqlite3")
+	os.system("C:\\Python27\\Scripts\\pip.exe install urllib")
+	os.system("C:\\Python27\\Scripts\\pip install pypiwin32")
+	os.system("C:\\Python27\\Scripts\\pip install logging")
+	os.system("C:\\Python27\\Scripts\\pip install logging")
+	os.system("C:\\Python27\\Scripts\\pip install pyCrypto")
 	urllib.urlretrieve("https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi", dir_path + "VCForPython27.msi")
 	os.system(dir_path + "VCForPython27.msi")
 	os.system("cls")
-<<<<<<< HEAD
-	
-=======
->>>>>>> 5cb263166273491c61b30c2042914cc1ebd8dc84
+
 ins()
 print """
 
